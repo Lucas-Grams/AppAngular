ec07b1389a2b1eb7033beb9bcd739b33ca77397c
Lucas Gabriel Grams de Souza <126901895+Lucas-Grams@users.noreply.github.com>
2023-03-23 08:35:43 -0300

    configurações da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photos/photo/photo.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent,
+    PhotoFormComponent,
+    PhotoListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
