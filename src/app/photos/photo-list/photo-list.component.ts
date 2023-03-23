1753e5052f76238bff752cc4985ac1c0eeaa9e32
Lucas Gabriel Grams de Souza <126901895+Lucas-Grams@users.noreply.github.com>
2023-03-23 11:34:22 -0300

    adicionando caixa de pesquisa nas fotos

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -12,14 +12,23 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = []; 
+
+  filter:string = '';
   
   constructor(
     private photoService: PhotoService,
     private activatedRoute: ActivatedRoute
   ) { }
 
+  onKeyUp(target : any) {
+    if(target instanceof EventTarget) {
+      var elemento = target as HTMLInputElement;
+      this.filter = elemento.value;
+    }
+  }
+
   ngOnInit(): void {
-    
+
       const userName = this.activatedRoute.snapshot.params['userName'];
 
       this.photoService
--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -17,6 +17,7 @@ export class PhotosComponent implements OnChanges{
   ngOnChanges(changes: SimpleChanges){
     if(changes['photos']){
        this.rows = this.groupColumns(this.photos);
+       console.log(this.rows.length)
     }
    }
 
