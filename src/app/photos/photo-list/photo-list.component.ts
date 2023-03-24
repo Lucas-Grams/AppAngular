436ce75ed58cb3a84ef6d77a64f9b09b46268657
Lucas Gabriel Grams de Souza <126901895+Lucas-Grams@users.noreply.github.com>
2023-03-24 09:04:08 -0300

    criamos um resolver pra pagina

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 
-import { PhotoService } from '../photo/photo.service';
+
 
 @Component({
   selector: 'app-photo-list',
@@ -15,10 +15,7 @@ export class PhotoListComponent implements OnInit {
 
   filter:string = '';
   
-  constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   onKeyUp(target : any) {
     if(target instanceof EventTarget) {
@@ -28,11 +25,6 @@ export class PhotoListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
-      const userName = this.activatedRoute.snapshot.params['userName'];
-
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
 }
