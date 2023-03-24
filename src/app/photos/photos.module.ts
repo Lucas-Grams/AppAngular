9fcad8e0dfe9218fcafd11bcdbe44a85dfb97300
Lucas Gabriel Grams de Souza <126901895+Lucas-Grams@users.noreply.github.com>
2023-03-24 14:15:43 -0300

    adição de links

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-
 
 import { PhotoModule } from './photo/photo.module';
 import { PhotoFormModule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
-
 
 @NgModule({
     declarations: [ ],
@@ -14,7 +10,6 @@ import { PhotoListModule } from './photo-list/photo-list.module';
         PhotoModule,
         PhotoFormModule,
         PhotoListModule,
-
     ]
 })
 export class PhotosModule {}
